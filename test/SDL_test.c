686981b7ffd7e776a1ede7d9c2d6ea41213105e8
Darrin-Lin <darrin.de88@gmail.com>
2024-05-12 20:25:37 +0800

    test: Test SDL ttf.

--- test/SDL_test.c
+++ test/SDL_test.c
@@ -1,5 +1,7 @@
 #include "../src/SDL_porcess/init_all_SDL.h"
 #include "../src/SDL_porcess/SDL_include.h"
+#define DEBUG
+#include "../src/debug.h"
 void apply_surface(int x, int y, SDL_Surface *source, SDL_Surface *destination)
 {
     SDL_Rect offset;
@@ -31,8 +33,27 @@ int main(int argc, char *argv[])
     SDL_Rect rect1 = {150, 150, 200, 200};
 
     TTF_Font *font = TTF_OpenFont("../ttf/NotoSansTC-Medium.ttf", 32);
+    if(font==NULL)
+    {
+        SDL_DestroyTexture(texture);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        debug_print("font is NULL");
+        return -1;
+    }
     SDL_Color color = {0xFF, 0xFF, 0xFF, 0xFF};
+
     message = TTF_RenderText_Solid(font, "The quick brown fox jumps over the lazy dog", color);
+    if (message == NULL)
+    {
+        SDL_DestroyTexture(texture);
+        SDL_DestroyRenderer(renderer);
+        SDL_DestroyWindow(window);
+        debug_print("message is NULL");
+        return -1;
+    }
+    SDL_Texture *mTexture;
+    mTexture = SDL_CreateTextureFromSurface(renderer, message);
 
     while (1)
     {
@@ -40,16 +61,20 @@ int main(int argc, char *argv[])
         {
             if (event.type == SDL_QUIT)
             {
+                SDL_DestroyTexture(mTexture);
                 SDL_DestroyTexture(texture);
                 SDL_DestroyRenderer(renderer);
                 SDL_DestroyWindow(window);
+                SDL_DestroyTexture(message);
+                TTF_CloseFont(font);
                 SDL_Quit();
                 return 0;
             }
         }
-        SDL_RenderClear(renderer);
+        // SDL_RenderClear(renderer);
         apply_surface(0, 150, message, window);
-        SDL_RenderCopy(renderer,message,NULL,&rect);
+        SDL_RenderCopy(renderer,mTexture,NULL,&rect1);  
+        SDL_RenderCopy(renderer, texture, NULL, &rect1);  
         // SDL_RenderCopy(renderer, texture, NULL, &rect);
         SDL_RenderPresent(renderer);
     }
