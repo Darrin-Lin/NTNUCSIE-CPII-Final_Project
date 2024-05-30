56276b3372e13ad46f718759ef19c5526d0fe6d4
Darrin-Lin <darrin.de88@gmail.com>
2024-05-31 00:18:32 +0800

    feat: Add favorability save functions.

--- src/cJSON_process/save_process.c
+++ src/cJSON_process/save_process.c
@@ -2,7 +2,7 @@
 
 int8_t update_save_file(char *save_path, cJSON *save)
 {
-    if(save == NULL)
+    if (save == NULL)
     {
         debug_print("save is empty\n");
         return -1;
@@ -19,7 +19,7 @@ int8_t update_save_file(char *save_path, cJSON *save)
 }
 int8_t update_event(cJSON *save, char *event_id)
 {
-    if(save == NULL || event_id == NULL)
+    if (save == NULL || event_id == NULL)
     {
         debug_print("save or event_id is empty\n");
         return -1;
@@ -39,7 +39,7 @@ int8_t update_event(cJSON *save, char *event_id)
 
 int8_t update_add_item(cJSON *save, char *item_id)
 {
-    if(save == NULL || item_id == NULL)
+    if (save == NULL || item_id == NULL)
     {
         debug_print("save or item_id is empty\n");
         return -1;
@@ -61,7 +61,7 @@ int8_t update_add_item(cJSON *save, char *item_id)
 
 int8_t update_remove_item(cJSON *save, char *item_id)
 {
-    if(save == NULL || item_id == NULL)
+    if (save == NULL || item_id == NULL)
     {
         debug_print("save or item_id is empty\n");
         return -1;
@@ -74,7 +74,7 @@ int8_t update_remove_item(cJSON *save, char *item_id)
     }
     else
     {
-        for(int i = 0; i < cJSON_GetArraySize(items); i++)
+        for (int i = 0; i < cJSON_GetArraySize(items); i++)
         {
             cJSON *item = cJSON_GetArrayItem(items, i);
             if (strcmp(item->valuestring, item_id) == 0)
@@ -90,7 +90,7 @@ int8_t update_remove_item(cJSON *save, char *item_id)
 
 int8_t update_check_item(cJSON *save, char *item_id)
 {
-    if(save == NULL || item_id == NULL)
+    if (save == NULL || item_id == NULL)
     {
         debug_print("save or item_id is empty\n");
         return 0;
@@ -103,7 +103,7 @@ int8_t update_check_item(cJSON *save, char *item_id)
     }
     else
     {
-        for(int i = 0; i < cJSON_GetArraySize(items); i++)
+        for (int i = 0; i < cJSON_GetArraySize(items); i++)
         {
             cJSON *item = cJSON_GetArrayItem(items, i);
             if (strcmp(item->valuestring, item_id) == 0)
@@ -113,5 +113,116 @@ int8_t update_check_item(cJSON *save, char *item_id)
         }
     }
     debug_print("item not found\n");
+    return 0;
+}
+int8_t update_get_item_num(cJSON *save, int32_t *item_num)
+{
+    if (save == NULL)
+    {
+        debug_print("save is empty\n");
+        return -1;
+    }
+    cJSON *items = cJSON_GetObjectItem(save, "item");
+    if (items == NULL)
+    {
+        debug_print("item is empty\n");
+        *item_num = 0;
+    }
+    else
+    {
+        *item_num = cJSON_GetArraySize(items);
+    }
+    return 0;
+}
+
+
+int8_t update_favorability_add(cJSON *save, char *character_id, int32_t favorability_add)
+{
+    if (save == NULL || character_id == NULL)
+    {
+        debug_print("save or character_id is empty\n");
+        return -1;
+    }
+    cJSON *favorability = cJSON_GetObjectItem(save, "favorability");
+    if (favorability == NULL)
+    {
+        debug_print("favorability is empty\n");
+        return -1;
+    }
+    else
+    {
+        cJSON *character = cJSON_GetObjectItem(favorability, character_id);
+
+        if (character == NULL)
+        {
+            debug_print("character not found\n");
+            cJSON_AddNumberToObject(favorability, character_id, 0 + favorability_add);
+        }
+        else
+        {
+            cJSON_SetValueint(character, cJSON_GetNumberValue(character) + favorability_add);
+        }
+        return 0;
+    }
+    debug_print("character not found\n");
+    return -1;
+}
+
+int8_t update_favorability_get(cJSON *save, char *character_id, int32_t *favorability)
+{
+    if (save == NULL || character_id == NULL)
+    {
+        debug_print("save or character_id is empty\n");
+        return -1;
+    }
+    cJSON *favorability_json = cJSON_GetObjectItem(save, "favorability");
+    if (favorability_json == NULL)
+    {
+        debug_print("favorability not empty\n");
+        cJSON_AddItemToObject(save, "favorability", cJSON_CreateObject());
+        favorability_json = cJSON_GetObjectItem(save, "favorability");
+        cJSON_AddNumberToObject(favorability_json, character_id, 0);
+        *favorability = 0;
+    }
+    else
+    {
+        cJSON *character = cJSON_GetObjectItem(favorability_json, character_id);
+        if (character == NULL)
+        {
+            debug_print("character not found\n");
+            cJSON_AddNumberToObject(favorability_json, character_id, 0);
+        }
+        *favorability = cJSON_GetNumberValue(character);
+    }
+    return 0;
+}
+int8_t update_favorability_set(cJSON *save, char *character_id, int32_t favorability)
+{
+    if (save == NULL || character_id == NULL)
+    {
+        debug_print("save or character_id is empty\n");
+        return -1;
+    }
+    cJSON *favorability_json = cJSON_GetObjectItem(save, "favorability");
+    if (favorability_json == NULL)
+    {
+        debug_print("favorability not empty\n");
+        cJSON_AddItemToObject(save, "favorability", cJSON_CreateObject());
+        favorability_json = cJSON_GetObjectItem(save, "favorability");
+        cJSON_AddNumberToObject(favorability_json, character_id, favorability);
+    }
+    else
+    {
+        cJSON *character = cJSON_GetObjectItem(favorability_json, character_id);
+        if (character == NULL)
+        {
+            debug_print("character not found\n");
+            cJSON_AddNumberToObject(favorability_json, character_id, favorability);
+        }
+        else
+        {
+            cJSON_SetValueint(character, favorability);
+        }
+    }
     return 0;
 }
--- src/cJSON_process/save_process.h
+++ src/cJSON_process/save_process.h
@@ -9,3 +9,8 @@ int8_t update_event(cJSON *save, char *event_id);
 int8_t update_add_item(cJSON *save, char *item_id);
 int8_t update_remove_item(cJSON *save, char *item_id);
 int8_t update_check_item(cJSON *save, char *item_id);
+int8_t update_get_item_num(cJSON *save, int32_t *item_num);
+int8_t update_favorability_add(cJSON *save, char *character_id, int32_t favorability_add);
+int8_t update_favorability_get(cJSON *save, char *character_id, int32_t *favorability);
+int8_t update_favorability_set(cJSON *save, char *character_id, int32_t favorability);
+
